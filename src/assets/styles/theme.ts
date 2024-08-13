7132e3ca66f0913450ca213e895bfca9edcc96bf
seiyeon <wsx4379@naver.com>
2024-08-13 16:06:09 +0900

    feat: theme.ts 추가, color 설정
    - color 설정
    - ThemeProvider 추가

--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -1,14 +1,24 @@
-export const light = {
-  bgColor: "rgb(255 255 255 / 1)",
-};
+import { DefaultTheme } from "styled-components";
 
-export const dark = {
-  bgColor: "rgb(23 23 23/1)",
+export const theme: DefaultTheme = {
+  color: {
+    main: "#FFC814",
+    mainLight: "#FFF8E0",
+    secondary: "#6979F8",
+    secondaryLight: "#E7E9FE",
+    pointRed: "#FF5757",
+    pointYellow: "#FFD990",
+    pointCoral: "#FF9E80",
+    white: "#FFFFFF",
+    black: "#000000",
+    background: "#FAFAFA",
+    gray200: "#E6E6E6",
+    gray300: "#B8B8B8",
+    gray400: "#999999",
+    gray500: "#808080",
+    gray600: "#666666",
+    gray700: "#4D4D4D",
+    gray800: "#333333",
+    gray900: "#1A1A1A",
+  },
 };
-
-const themes = {
-  light: light,
-  dark: dark,
-};
-
-export default themes;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./assets/fonts/font.css";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./assets/styles/theme.ts";
 
 createRoot(document.getElementById("root")!).render(
-  <>
+  <ThemeProvider theme={theme}>
     <App />
-  </>
+  </ThemeProvider>
 );
